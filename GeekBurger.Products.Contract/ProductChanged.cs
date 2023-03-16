318714849b5e1803733b0639595e36b209ff918c
Luiz Aguiar <luizmanoel00@gmail.com>
2023-03-15 22:11:20 -0300

    initial commit

---
+++ GeekBurger.Products.Contract/ProductChanged.cs
@@ -0,0 +1,14 @@
+namespace GeekBurger.Products.Contract;
+
+public class ProductChanged
+{
+    public ProductState State { get; set; }
+    public Product Product { get; set; }
+}
+
+public enum ProductState
+{
+    Deleted = 2,
+    Modified = 3,
+    Added = 4
+}
---
+++ GeekBurger.Products/Controllers/ProductsController.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GeekBurger.Products.Contract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekBurger.Products.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private IList<Product> Products = new List<Product>();
+
+        public ProductsController()
+        {
+            var paulistaStore = "Paulista";
+            var morumbiStore = "Morumbi";
+
+            var beef = new Item { ItemId = Guid.NewGuid(), Name = "beef" };
+            var pork = new Item { ItemId = Guid.NewGuid(), Name = "pork" };
+            var mustard = new Item { ItemId = Guid.NewGuid(), Name = "mustard" };
+            var ketchup = new Item { ItemId = Guid.NewGuid(), Name = "ketchup" };
+            var bread = new Item { ItemId = Guid.NewGuid(), Name = "bread" };
+            var wBread = new Item { ItemId = Guid.NewGuid(), Name = "whole bread" };
+
+            Products = new List<Product>()
+            {
+                new Product
+                {
+                    ProductId = Guid.NewGuid(), Name = "Darth Bacon",
+                    Image = "hamb1.png", StoreName = paulistaStore,
+                    Items = new List<Item> { beef, ketchup, bread }
+                },
+                new Product
+                {
+                    ProductId = Guid.NewGuid(), Name = "Cap. Spork",
+                    Image = "hamb2.png", StoreName = paulistaStore,
+                    Items = new List<Item> { pork, mustard, wBread }
+                },
+                new Product
+                {
+                    ProductId = Guid.NewGuid(), Name = "Beef Turner",
+                    Image = "hamb3.png", StoreName = morumbiStore,
+                    Items = new List<Item> { beef, mustard, bread }
+                }
+            };
+        }
+
+        [HttpGet]
+        public IActionResult GetProductsByStoreName(string storeName)
+        {
+            var productsByStore = Products.Where(product =>
+                product.StoreName.ToUpper() == storeName.ToUpper()).ToList();
+
+            if (productsByStore.Count <= 0)
+                return NotFound();
+
+            return Ok(productsByStore);
+        }
+    }
+}
